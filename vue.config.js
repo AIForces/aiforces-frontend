97a077c1253c90f54f55e06db080f5deaac66d80
Nikolay Rulev <nikolayr2001@gmail.com>
2019-07-04 00:23:47 +0300

    fix proxy config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   devServer: {
-    proxy: 'http://localhost:3000',
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:3000',
+        ws: true,
+        changeOrigin: true,
+      },
+    },
   },
 };
